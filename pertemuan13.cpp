dcd4017428b4e495fe440f5b89ef6a64e77baba9
arif rafinda rahman <arifrafidarahman@gmail.com>
2024-11-24 00:07:22 +0700

    menambahkan file cpp pertemuan 13

---
+++ pertemuan13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int genap[5] = {2,4,6,8,10};
+
+cout <<"array 0 = "<<genap[0]<<endl;
+cout <<"array 1 = "<<genap[1]<<endl;
+cout <<"array 2 = "<<genap[2]<<endl;
+cout <<"array 3 = "<<genap[3]<<endl;
+cout <<"array 4 = "<<genap[4]<<endl;
+
+}
